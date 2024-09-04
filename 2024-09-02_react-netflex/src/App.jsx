eebbf462a66769715b176d0f53cf2c1d6d7201f5
Jin Young, Yoo <yoojinyoung@kakao.com>
2024-09-04 17:32:15 +0900

    2024-09-04 17:32

--- 2024-09-02_react-netflex/src/App.jsx
+++ 2024-09-02_react-netflex/src/App.jsx
@@ -1,36 +1,16 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import styles from "./App.module.scss";
 import Header from "./components/Header/Header";
 import MoviesList from "./components/MoviesList/MoviesList";
 
 function App() {
-  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
-
-  const getMovies = async () => {
-    const headers = {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYjdiMTJjM2M2NjhiMjNjZThhNmNhMjFiYTE5M2JjYiIsIm5iZiI6MTcyNDgzMjQ5NC41NTE1NTMsInN1YiI6IjY1YTlkNjZjNTM0NjYxMDEzOGNkMTFhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rRVZwTNunIYGQ1-wPudD_JX_4KKTVWUSXtLP5Y4ARqs",
-    };
-    const url =
-      "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1";
-
-    const response = await axios.get(url, { headers });
-    const movies = response.data.results;
-
-    setNowPlayingMovies(movies);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
   return (
     <div className={styles.app}>
       <main>
         <Header />
-        <MoviesList title="현재 상영작" movies={nowPlayingMovies} />
-        <MoviesList title="평점이 높은 영화" movies={nowPlayingMovies} />
+        <MoviesList title="현재 상영작" category="now_playing" />
+        <MoviesList title="평점이 높은 영화" category="top_rated" />
+        <MoviesList title="인기 있는" category="popular" />
+        <MoviesList title="곧 개봉하는" category="upcoming" />
       </main>
     </div>
   );
--- 2024-09-02_react-netflex/src/components/MoviesList/MoviesList.jsx
+++ 2024-09-02_react-netflex/src/components/MoviesList/MoviesList.jsx
@@ -1,15 +1,24 @@
+import { useEffect, useState } from "react";
+import { getMoviesOnCategory } from "../../api/api";
 import MovieCard from "../MovieCard/MovieCard";
 import styles from "./MoviesList.module.scss";
 
-function MoviesList(props) {
-  const { title, movies } = props;
+function MoviesList({ title, category }) {
+  const [moviesOnCategory, setMoviesOnCategory] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const movies = await getMoviesOnCategory(category);
+      setMoviesOnCategory(movies);
+    })();
+  }, [category]);
 
   return (
     <section className={styles.section}>
       <h3 className={styles.sectionTitle}>{title}</h3>
 
       <ul className={styles.moviesList}>
-        {movies.map((movie) => (
+        {moviesOnCategory.map((movie) => (
           <li key={movie.id} className={styles.moviesListItem}>
             <MovieCard movie={movie} />
           </li>
